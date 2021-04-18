675d422fc9f56f339763071e6a420804e45c5f59
Anton Mishyn <mishinaa333@gmail.com>
2021-04-18 13:23:56 +0300

    Project structure and dependencies

---
+++ src/main/java/com/mishyn/proxy/Application.java
@@ -0,0 +1,13 @@
+package com.mishyn.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
